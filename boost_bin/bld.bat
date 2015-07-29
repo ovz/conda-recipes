d3405cab3dbd47341560c1a86ae1a5a66bc3d87d
ovz <ovz@salford-systems.com>
2015-07-29 15:44:05 -0700

    Reworked to take libraries from from default installation location of http://sourceforge.net/projects/boost/files/boost-binaries binaries installer.

--- boost_bin/bld.bat
+++ boost_bin/bld.bat
@@ -1,1 +1,1 @@
-xcopy /Y /I /E Win%ARCH%\* %SP_DIR%
+powershell -noProfile -Command "Copy-Boost -boost_bin_root C:\local\boost_1_57_0 -ARCH %ARCH% -SP_DIR %SP_DIR% -LIBRARY_LIB %LIBRARY_LIB% -LIBRARY_INC %LIBRARY_INC%"
