2e103d1e09fa32b1a02c0eb544ecfd1514bf8275
Vlad Frolov <frolvlad@gmail.com>
2016-04-15 13:19:23 +0700

    invoke: Fixed bld.bat syntax

--- invoke/bld.bat
+++ invoke/bld.bat
@@ -3,8 +3,11 @@
 :: Related question on conda-build issue tracker:
 :: https://github.com/conda/conda-build/pull/317#commitcomment-12926020
 :: WARNING: this has to be removed when noarch_python is enabled!
-if "%PY_VER%" == "2.7" rmdir /S /Q "invoke\vendor\yaml3"
-else rmdir /S /Q "invoke\vendor\yaml2"
+if "%PY_VER%" == "2.7" (
+    rmdir /S /Q "invoke\vendor\yaml3"
+) else (
+    rmdir /S /Q "invoke\vendor\yaml2"
+)
 
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
