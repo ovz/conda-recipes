312d91b674f740563de35bcaa9f5423a7e4ada23
ovz <ovz@salford-systems.com>
2016-05-24 08:43:21 -0700

    qwt: Copy mathml headers in include/qwt.
    This exposes mathml interface to client code.

--- qwt/bld.bat
+++ qwt/bld.bat
@@ -1,5 +1,8 @@
 set sub_dir=qwt
 xcopy /Y /I /E include\%sub_dir% %LIBRARY_INC%\%sub_dir% 
+
+xcopy /Y /I /E src\textengines\mathml\*.h %LIBRARY_INC%\%sub_dir%\mathml 
+
 copy include\*.pri %LIBRARY_INC%
 copy /Y Win\lib\* %LIBRARY_LIB%
 
--- qwt/build.sh
+++ qwt/build.sh
@@ -1,5 +1,9 @@
 cp -r include/* $PREFIX/include
 
+mathml_dir=$PREFIX/include/qwt/mathml
+mkdir -p $mathml_dir
+cp src/textengines/mathml/*.h $mathml_dir
+
 dst_dir=$PREFIX/lib
 mkdir -p $dst_dir
 cp $(uname)$ARCH/* $dst_dir
