862854002402466de23d3a1f99178cdef0bdfd4a
ovz <ovz@salford-systems.com>
2016-06-30 11:48:18 -0700

    msgpack-c: Commented out untested part of bld.bat.

--- msgpack-c/bld.bat
+++ msgpack-c/bld.bat
@@ -1,13 +1,19 @@
+REM COPY Header files for now
+
+
+xcopy /Y /I /E include %PREFIX%\include 
+
+
 REM WARNING: This script is not tested at all! Installation step is missing
 
-cmake -DCMAKE_INSTALL_PREFIX=%PREFIX%\Library ..
-if errorlevel 1 exit 1
+REM cmake -DCMAKE_INSTALL_PREFIX=%PREFIX%\Library ..
+REM if errorlevel 1 exit 1
 
 REM Building
-cmake --build .
-if errorlevel 1 exit 1
+REM cmake --build .
+REM if errorlevel 1 exit 1
 
 REM Installing
 REM make install
-exit 1
-if errorlevel 1 exit 1
+REM exit 1
+REM if errorlevel 1 exit 1
