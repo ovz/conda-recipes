95af05f9abef2a7d4d7bc8d0489d8e38c36fe4c6
Vlad Frolov <frolvlad@gmail.com>
2016-08-01 00:59:32 +0300

    gcc-6: Backported the changes introduced in upstream gcc-4.8 recipe

--- gcc-6/build.sh
+++ gcc-6/build.sh
@@ -6,6 +6,20 @@
 # "system" header path.
 # That could cause -I$PREFIX/include to be essentially ignored in users' recipes
 # (It would still be on the search path, but it would be in the wrong position in the search order.)
+# .. Unfortunately:
+#    1. Worked by relying on failure to relocate paths (until this commit).
+#    2. .. and then not finding unrelocated folders, worse, if build from /root/miniconda3 as linux-32 4.8.5-3 was
+#    3. .. led to 'Permission denied' failures from attempting to still access those unrelocated paths.
+#    The current implementation fixes it by making libiberty's relocation code capable of handling '..' and then
+#    configuring the various other (non-prefix) dirs as relative to '${GCC_PREFIX}/..'. This turns the two
+#    'Permission Denied' errors into 'ignoring duplicate directory'.
+# I disagree with the motivation behind this change. What it does is prevents #include <GL/gl.h> from finding
+#  Conda's version of gl.h, should we wish to use a custom one (angleproject for example) unless the user
+#  specifies -I${PREFIX}/include, and when they do that, that path is added to the *front* of the system
+#  includes path, which is *not* where it is meant to be. It is meant to appear between /usr/local/include and
+#  /usr/include as a block of 3 final system includes, and all of that comes after GCCs own headers and those
+#  from libstdc++. I will revist this later.
+
 GCC_PREFIX="$PREFIX/gcc"
 mkdir "$GCC_PREFIX"
 
@@ -14,15 +28,15 @@ ln -s "$PREFIX/lib" "$PREFIX/lib64"
 if [ "$(uname)" == "Darwin" ]; then
     # On Mac, we expect that the user has installed the xcode command-line utilities (via the 'xcode-select' command).
     # The system's libstdc++.6.dylib will be located in /usr/lib, and we need to help the gcc build find it.
-    export LDFLAGS="-Wl,-headerpad_max_install_names,-rpath,${PREFIX}/lib -Wl,-L${PREFIX}/lib -Wl,-L/usr/lib"
+    export LDFLAGS="-Wl,-headerpad_max_install_names -Wl,-L${PREFIX}/lib -Wl,-L/usr/lib"
     export DYLD_FALLBACK_LIBRARY_PATH="$PREFIX/lib:/usr/lib"
 
     ./configure \
         --prefix="$GCC_PREFIX" \
         --with-gxx-include-dir="$GCC_PREFIX/include/c++" \
-        --bindir="$PREFIX/bin" \
-        --datarootdir="$PREFIX/share" \
-        --libdir="$PREFIX/lib" \
+        --bindir="$GCC_PREFIX/../bin" \
+        --datarootdir="$GCC_PREFIX/../share" \
+        --libdir="$GCC_PREFIX/../lib" \
         --with-gmp="$PREFIX" \
         --with-mpfr="$PREFIX" \
         --with-mpc="$PREFIX" \
@@ -36,17 +50,22 @@ else
     # For reference during post-link.sh, record some
     # details about the OS this binary was produced with.
     mkdir -p "${PREFIX}/share"
-    cat /etc/*-release > "${PREFIX}/share/conda-gcc-build-machine-os-details"
+
+    # lsb_release can complain about LSB modules in stderr, so we
+    # ignore that.
+    lsb_release -a 1> "$PREFIX/share/conda-gcc-build-machine-os-details"
+
     ./configure \
         --prefix="$GCC_PREFIX" \
         --with-gxx-include-dir="$GCC_PREFIX/include/c++" \
-        --bindir="$PREFIX/bin" \
-        --datarootdir="$PREFIX/share" \
-        --libdir="$PREFIX"/lib \
+        --bindir="$GCC_PREFIX/../bin" \
+        --datarootdir="$GCC_PREFIX/../share" \
+        --libdir="$GCC_PREFIX/../lib" \
         --with-gmp="$PREFIX" \
         --with-mpfr="$PREFIX" \
         --with-mpc="$PREFIX" \
         --with-isl="$PREFIX" \
+        --with-cloog="$PREFIX" \
         --enable-checking=release \
         --with-tune=generic \
         --disable-multilib
@@ -63,5 +82,9 @@ make install-strip
 
 rm "$PREFIX/lib64"
 
-# Link cc to gcc if cc doesn't exist
-[ -e "$PREFIX/bin/cc" ] || ln -s "gcc" "$PREFIX/bin/cc"
+# Fix libtool paths
+find "$PREFIX" -name '*.la' -print0 | xargs -0  sed -i.backup 's%lib/../lib64%lib%g'
+find "$PREFIX" -name '*la.backup' -print0 | xargs -0  rm -f
+
+# Link cc to gcc
+(cd "$PREFIX"/bin && ln -s gcc cc)
